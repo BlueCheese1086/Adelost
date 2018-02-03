bcf53c83dfb71ec716d9e7b8fbe94f0bdb366e9d
James <errorshadow7@gmail.com>
2018-02-03 10:25:30 -0500

    Wrote code for intake

--- src/org/usfirst/frc/team1086/subsystems/Intake.java
+++ src/org/usfirst/frc/team1086/subsystems/Intake.java
@@ -1,4 +1,22 @@
 package org.usfirst.frc.team1086.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 public class Intake {
+	TalonSRX intake1; 
+	TalonSRX intake2;
+	
+	public Intake() {
+		intake1 = new TalonSRX(0); // Filler numbers for motor ports 
+		intake2 = new TalonSRX(0);
+	}
+	public void motorIn() {
+		intake1.set(ControlMode.PercentOutput, 0.5);
+		intake2.set(ControlMode.PercentOutput, 0.5);
+	}
+	public void motorOff() {
+		intake1.set(ControlMode.PercentOutput, 0.0);
+		intake2.set(ControlMode.PercentOutput, 0.0);
+	}
 }
