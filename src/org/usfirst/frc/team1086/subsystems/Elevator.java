a2eb866c9a08b3d9402d9a900be18b734e3cc20e
Kevin Ngo <Kevin.ngo0407@gmail.com>
2018-03-03 14:26:08 -0500

    merge conflict

--- src/org/usfirst/frc/team1086/subsystems/Elevator.java
+++ src/org/usfirst/frc/team1086/subsystems/Elevator.java
@@ -53,7 +53,8 @@ public class Elevator implements Tickable {
         }*/
         Globals.elevatorHeight.setDouble(encToInches(elevatorMotor.getSelectedSensorPosition(0)));
         SmartDashboard.putNumber("Target Height", targetHeight);
-        SmartDashboard.putNumber("Current", elevatorMotor.getOutputCurrent());
+        SmartDashboard.putNumber("Current 1", elevatorMotor.getOutputCurrent());
+        SmartDashboard.putNumber("Current 2", elevatorFollower.getOutputCurrent());
         if(inputManager.getElevatorSafety()) {
             if(inputManager.getElevatorOverride()){
                 elevatorMotor.set(ControlMode.PercentOutput, inputManager.getElevator() * 2 - 1);
